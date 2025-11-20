689e5782c963fe755628155445bcb4087c394e90
Alex Sanchez-Olvera <alex.sanchez-olvera@goldenhippo.com>
2025-11-20 11:11:44 -0800

    fix: fix Header responsiveness

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideSearch } from '@ng-icons/lucide';
+import { lucideMenu, lucideSearch } from '@ng-icons/lucide';
 import { HeaderUserButtonComponent } from './header-user-button/header-user-button.component';
 import { HeaderCartButtonComponent } from './header-cart-button/header-cart-button.component';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [NgIcon, HeaderUserButtonComponent, HeaderCartButtonComponent],
-  providers: [provideIcons({ lucideSearch })],
+  providers: [provideIcons({ lucideMenu, lucideSearch })],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
